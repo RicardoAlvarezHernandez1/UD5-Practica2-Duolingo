5769fba44ff2ed1a0846eff3d0fa3a3f938d263b
RicardoAlvarezHernandez1 <rica07b@gmail.com>
2023-05-15 21:25:49 +0100

    feat(Diccionario) : new methods eliminarPalabra(String) and existePalabra(String)

--- src/net/salesianos/duolingo/almacendepalabras/Diccionario.java
+++ src/net/salesianos/duolingo/almacendepalabras/Diccionario.java
@@ -10,8 +10,8 @@ public class Diccionario {
     private Map <String , Set<String>> diccionario = new HashMap<>();
 
     public void annadirPalabra(String palabra){
-        String inicial = palabra.substring(0 , 1);
         palabra = palabra.toLowerCase().trim();
+        String inicial = palabra.substring(0 , 1);
         if( diccionario.containsKey(inicial)){
             Set<String> palabras1 = diccionario.get(inicial);
             palabras1.add(palabra);
@@ -24,9 +24,39 @@ public class Diccionario {
         System.out.println(diccionario);
     }
 
+    public void eliminarPalabra(String palabra){
+        String inicial = palabra.substring(0 , 1);
+        palabra = palabra.toLowerCase().trim();
+        if( diccionario.containsKey(inicial)){
+            Set<String> palabras1 = diccionario.get(inicial);
+            palabras1.remove(palabra);
+            if (palabras1.size() == 0) {
+                diccionario.remove(inicial);
+            }
+        } else {
+            System.out.println("La palabra no estaba almacenada");
+        }
+        System.out.println(diccionario);
+    }
 
+    public void existePalabra(String palabra){
+        String inicial = palabra.substring(0 , 1);
+        palabra = palabra.toLowerCase().trim();
+        if (diccionario.containsKey(inicial)) {
+            Set<String> palabras1 = diccionario.get(inicial);
+            if (palabras1.contains(palabra)) {
+                System.out.println("La palabra está almacenada"); 
+            } else {
+                System.out.println("La palabra no esta almacenada");
+            }
+        } else {
+            System.out.println("La palabra no esta almacenada");
+        }
+    }
     
 
    
     
+    
 }
+
